c7a240ca16da1e33933ecb1c0089ebbae7d4c329
KeanuTastenhoye <keanu.tastenhoye@student.ucll.be>
2019-09-26 14:25:09 +0200

    Caesar done

---
+++ Stategy-Keanu_Phloy_Eline/src/domain/Cypher.java
@@ -0,0 +1,8 @@
+package domain;
+
+public interface Cypher {
+
+    String encode(String s);
+
+    String decode(String s);
+}
---
+++ Stategy-Keanu_Phloy_Eline/src/domain/Mirroring.java
@@ -0,0 +1,14 @@
+package domain;
+
+public class Mirroring implements Cypher {
+
+    @Override
+    public String encode(String s) {
+        return null;
+    }
+
+    @Override
+    public String decode(String s) {
+        return null;
+    }
+}
