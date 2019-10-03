6bb51b2ff2171a8399fe6fff44578cc7fa14c9a6
elineschepers <39278079+elineschepers@users.noreply.github.com>
2019-10-03 14:29:37 +0200

    partyrental changed

--- labo3_state/src/domain/PartyMaterialRental.java
+++ labo3_state/src/domain/PartyMaterialRental.java
@@ -1,4 +1,40 @@
 package domain;
 
+import java.util.TreeSet;
+
 public class PartyMaterialRental {
+
+    private TreeSet<PartyArtikel> artikelen;
+
+
+    public PartyMaterialRental()
+    {
+        artikelen=new TreeSet<>();
+    }
+
+    public void addArtikel(PartyArtikel artikel)
+    {
+        if(artikel!=null)
+        {
+            artikelen.add(artikel);
+            artikel.setState(new LoanableState());
+        }
+    }
+    public void loanOut(PartyArtikel artikel)
+    {
+        if(artikelen.contains(artikel))
+        {
+            artikel.setState(new LoanedState());
+        }
+        else
+            {
+                throw new IllegalArgumentException("Dit artikel bestaat niet in ons assortiment");
+            }
+    }
+
+    public void returnArtikel(PartyArtikel artikel)
+    {
+        if(artikel.damagedState!=null){artikel.setState(new DamagedState());}
+        else{artikel.setState(new LoanableState());}
+    }
 }
