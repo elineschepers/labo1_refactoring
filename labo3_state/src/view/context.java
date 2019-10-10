608dcc93475c0668defcce11d60d7be07c1587b9
Keanu Tastenhoye <keanu.tastenhoye@student.ucll.be>
2019-10-10 14:57:22 +0200

    Context

--- labo3_state/src/view/context.java
+++ labo3_state/src/view/context.java
@@ -1,4 +1,24 @@
 package view;
 
+import domain.PartyArtikel;
+import domain.PartyMaterialRental;
+
+import javax.swing.*;
+
 public class context {
+    public static  void main(String[] args) {
+
+
+        PartyMaterialRental rental=new PartyMaterialRental();
+
+        String keuze = JOptionPane.showInputDialog("1. leen artikel \n2. breng artikel terug\n3. maak artikel\n\n0. Quit");
+        String text = JOptionPane.showInputDialog("Tekst?").toLowerCase();
+
+
+        //String choiceString = JOptionPane.showInputDialog(optie);
+
+        if (keuze.equals("1")) {// encode
+
+        }
+    }
 }
